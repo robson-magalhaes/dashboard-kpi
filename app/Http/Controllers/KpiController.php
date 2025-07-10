769ff0174160f8b3271b9bb92dd525a3c292a997
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2025-07-10 09:28:31 -0300

    Ajustado KpiController para retornar os dados da Kpi do banco de dados, também foi removido migration nao usada

--- app/Http/Controllers/KpiController.php
+++ app/Http/Controllers/KpiController.php
@@ -2,36 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kpi;
+
 class KpiController extends Controller
 {
     public function index()
     {
-        $kpis = [
-            [
-                'id' => 1,
-                'titulo' => 'Vendas do Dia',
-                'valor' => 1280.50,
-                'variacao_percentual' => 5.2
-            ],
-            [
-                'id' => 2,
-                'titulo' => 'Visitas do Site',
-                'valor' => 3421,
-                'variacao_percentual' => -3.4
-            ],
-            [
-                'id' => 3,
-                'titulo' => 'Novos Usuários',
-                'valor' => 28,
-                'variacao_percentual' => 12.1
-            ],
-            [
-                'id' => 4,
-                'titulo' => 'Pedidos Finalizados',
-                'valor' => 56,
-                'variacao_percentual' => 2.3
-            ]
-        ];
+        $kpis = Kpi::all();
 
         return response()->json($kpis);
     }
